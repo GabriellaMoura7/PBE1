f60faa3e6d19e17a46f3c23e85225094aa2e3aee
Gabriella Del Vigna de Moura <159180575+GabriellaMoura7@users.noreply.github.com>
2024-08-20 13:32:10 -0300

    Add files via upload

---
+++ Atividade02/Conta/src/prjAula04.java
@@ -0,0 +1,12 @@
+package prjAula04;
+public class prjAula04 {
+
+	public static void main(String[] args) {
+		
+		Conta novaConta01 = new Conta(0001, "Michelly, 10000.00");
+		
+		novaConta.ge
+
+	}
+
+}
